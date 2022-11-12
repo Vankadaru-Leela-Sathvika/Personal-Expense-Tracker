31d12b9f17884de6bb0156db65cb458926c7a14e
Leela Sathvika Vankadaru <69385943+Vankadaru-Leela-Sathvika@users.noreply.github.com>
2022-11-12 10:08:42 +0530

    sprint-1

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask,  render_template,request,redirect
-import sqlite3 as sql
+from flask import Flask,flash, render_template,request,redirect
+import credentials
 import ibm_db
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
+
+conn = ibm_db.connect("DATABASE="+credentials.DB2_DATABASE_NAME+";HOSTNAME="+credentials.DB2_HOST_NAME+";PORT="+credentials.DB2_PORT+";SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID="+credentials.DB2_UID+";PWD="+credentials.DB2_PWD+"",'','')
+
+cos = ibm_boto3.resource("s3",
+    ibm_api_key_id=credentials.COS_API_KEY_ID,
+    ibm_service_instance_id=credentials.COS_INSTANCE_CRN,
+    config=Config(signature_version="oauth"),
+    endpoint_url=credentials.COS_ENDPOINT
+)
+
+class User:
+    def __init__(self) -> None:
+        pass
 
 app = Flask(__name__)
+app.secret_key = "FlaskNotFoundError"
 
 @app.route('/')
 def index():
@@ -32,3 +48,44 @@ def presentExpenses():
 @app.route('/sample')
 def presentSample():
     return render_template('sample.html')
+
+
+@app.route('/postSignUpData',methods =['POST','GET'])
+def postSignUpData():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        
+        sql = "SELECT * FROM user WHERE email=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,email)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        if account:
+            return render_template('signup.html', msg="You are already a member, please login using your details")
+        else:
+            insert_sql = "INSERT INTO user(email,password,name) VALUES (?,?,?)"
+            prep_stmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(prep_stmt, 1, email)
+            ibm_db.bind_param(prep_stmt, 2, password)
+            ibm_db.bind_param(prep_stmt, 3, name)
+            ibm_db.execute(prep_stmt)
+        return render_template('signin.html', msg="Registration successfull..")
+
+@app.route('/postSignInData',methods =['POST','GET'])
+def postSignInData():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+    
+        sql = "SELECT PASSWORD FROM user WHERE email=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,email)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
+        if account and account["PASSWORD"]==password:
+            return render_template('home.html',email=email)
+        else:
+            return render_template('signin.html', invalidLogin="Your email or password is wrong!!")
